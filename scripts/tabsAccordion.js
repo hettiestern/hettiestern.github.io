d8d6604df4ff30936de1400778e197bdcc9b9ee9
hettiestern <82908308+hettiestern@users.noreply.github.com>
2021-04-20 15:23:39 -0500

    Add files via upload

---
+++ scripts/tabsAccordion.js
@@ -0,0 +1,60 @@
+//TABS
+//credit: w3schools.com how-to example for tabs (modified)
+
+//accessibility????
+
+
+function openTab(evt, tabName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		 tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the link that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+	
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+
+//ACCORDION TEXT
+//credit: w3schools.com how-to example for accordion text
+//added: aria attributes
+
+
+//accessibility????
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+      this.setAttribute("aria-expanded", "false")
+    } else {
+      panel.style.display = "block";
+      this.setAttribute("aria-expanded", "true")
+    }
+  });
+}
